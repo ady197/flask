6d0859ca928e911119eb65b172a6a21915d0ee06
adrian <ungureanu.adrian97@yahoo.com>
2018-12-14 18:15:48 +0200

    chapter-3-templates

--- src/flask/app_flasky/hello.py
+++ src/flask/app_flasky/hello.py
@@ -2,16 +2,23 @@ from flask import Flask
 from flask import render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
+
 from app_flasky.config import configuration
 
 app = Flask(__name__)
 app.config.from_object(configuration)
-manager = Manager(app)
 bootstrap = Bootstrap(app)
+manager = Manager(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index_with_user.html')
+    return render_template('index.html',
+                            current_time=datetime.utcnow())
+    #return render_template('index_with_user.html')
     #return render_template('index.html')
 
 @app.route('/user/<name>')
