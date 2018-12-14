16ffa9408ef44a834548d3cfc801746126956b67
adrian <ungureanu.adrian97@yahoo.com>
2018-12-14 18:03:21 +0200

    chapter3-some templates examples

--- src/flask/app_flasky/hello.py
+++ src/flask/app_flasky/hello.py
@@ -1,18 +1,24 @@
 from flask import Flask
+from flask import render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 from app_flasky.config import configuration
 
 app = Flask(__name__)
 app.config.from_object(configuration)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index_with_user.html')
+    #return render_template('index.html')
 
 @app.route('/user/<name>')
 def hello(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user_bootstrap.html',name=name)
+    #return render_template('index_with_user.html',user=name)
+    #return render_template('user.html',name=name)
 
 
 if __name__ == '__main__':
